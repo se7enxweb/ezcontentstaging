a753daaa9f0505bf7c8b03552053f3f093fd6c5f
7x <info@se7enx.com>
2024-01-28 23:26:21 -0800

    Update ezinfo.php updated package vendor and version number

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@
 *
 * @package ezcontentstaging
 *
-* @copyright Copyright (C) 2011-2013 eZ Systems AS. All rights reserved.
+* @copyright Copyright (C) 1999 - 2024 7x. 2011 - 2013 eZ Systems AS. All rights reserved.
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 */
 
@@ -12,10 +12,10 @@ class ezcontentstagingModuleInfo
 {
     function info()
     {
-        return array( 'Name' => "eZ Publish Content Staging extension (CP version)",
-                      'Version' => "1.2-dev",
-                      'Copyright' => "Copyright (C) 2011-2013 eZ Systems AS. All rights reserved.",
-                      'License' => "GNU General Public License v2.0"
+        return array( 'Name' => "eZ Publish Content Staging extension",
+                      'Version' => "1.2.1",
+                      'Copyright' => "Copyright (C) 1999 - 2024 7x. 2011 - 2013 eZ Systems AS. All rights reserved.",
+                      'License' => "GNU General Public License v2.0 or later"
                      );
     }
 }
